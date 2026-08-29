cad97aabe44c84fd1cc41c179bcf72add24feb6a
髙橋 伸多 <takahashi.1994715@gmail.com>
2026-08-29 17:11:43 +0900

    本追加画面のUIを作成
    * タイトル入力、著者入力、ステータス選択（ドロップダウンも実装）、登録ボタン

--- app/src/main/java/com/example/mybookshelf/ui/screen/bookadd/BookAddScreen.kt
+++ app/src/main/java/com/example/mybookshelf/ui/screen/bookadd/BookAddScreen.kt
@@ -1,11 +1,112 @@
 package com.example.mybookshelf.ui.screen.bookadd
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mybookshelf.ui.theme.MyBookShelfTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookAddScreen() {
-    Text(
-        text = "本を追加"
-    )
+    var title by remember { mutableStateOf("") }
+    var author by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+    var expanded by remember { mutableStateOf(false) }
+    val statusList = listOf("未読", "読書中", "読了")
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(text = "本を追加")
+
+        OutlinedTextField(
+            value = title,
+            onValueChange = { title = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("タイトル") }
+        )
+
+        OutlinedTextField(
+            value = author,
+            onValueChange = { author = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("著者") }
+        )
+
+        Text("ステータス")
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            TextField(
+                value = status,
+                onValueChange = {},
+                readOnly = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .menuAnchor(type = ExposedDropdownMenuAnchorType.PrimaryNotEditable),
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = expanded
+                    )
+                }
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                statusList.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option) },
+                        onClick = {
+                            status = option
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+
+        Button(
+            onClick = {
+                // 後で登録処理を実装
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("登録")
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BookListScreenPreview() {
+    MyBookShelfTheme {
+        BookAddScreen()
+    }
 }
